d31a890b606a9d3919a8a1063c4b2a5d288d5191
Krishnanshu <krishnanshu.khanna@gmail.com>
2025-08-29 11:27:12 +0530

    feat: initialize project with Express server and /bfhl endpoint
    - Created package.json with Express as a dependency
    - Implemented server.js to handle POST requests at /bfhl
    - Processed input data to categorize numbers, alphabets, and special characters
    - Returned structured JSON response including user details and processed data

---
+++ server.js
@@ -0,0 +1,54 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/bfhl", (req, res) => {
+	try {
+		const data = req.body.data || [];
+		let evens = [],
+			odds = [],
+			alphabets = [],
+			specials = [],
+			sum = 0,
+			letters = [];
+
+		data.forEach((item) => {
+			if (/^-?\d+$/.test(item)) {
+				let num = parseInt(item);
+				if (num % 2 === 0) evens.push(item);
+				else odds.push(item);
+				sum += num;
+			} else if (/^[a-zA-Z]+$/.test(item)) {
+				alphabets.push(item.toUpperCase());
+				letters.push(item);
+			} else {
+				specials.push(item);
+			}
+		});
+
+		let concat = letters
+			.join("")
+			.split("")
+			.reverse()
+			.map((c, i) => (i % 2 === 0 ? c.toUpperCase() : c.toLowerCase()))
+			.join("");
+
+		res.status(200).json({
+			is_success: true,
+			user_id: "krishnanshu_khanna_12102004", 
+			email: "krishnanshu.khanna2022@vitstudent.ac.in",
+			roll_number: "22BCT0046",
+			odd_numbers: odds,
+			even_numbers: evens,
+			alphabets: alphabets,
+			special_characters: specials,
+			sum: sum.toString(),
+			concat_string: concat,
+		});
+	} catch (e) {
+		res.status(400).json({ is_success: false, error: e.message });
+	}
+});
+
+app.listen(3000, () => console.log("Server running on 3000"));
