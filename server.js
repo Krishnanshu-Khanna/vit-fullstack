edb6f43558025fc1bd44c28835900eb0722ff540
Krishnanshu <krishnanshu.khanna@gmail.com>
2025-08-29 11:29:45 +0530

    feat: add root endpoint returning a welcome message

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Hello from BFHL! 22BCT0046" });
+});
+
 app.post("/bfhl", (req, res) => {
 	try {
 		const data = req.body.data || [];
